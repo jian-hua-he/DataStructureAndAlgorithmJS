f4b93f03c24de229f0a4e0a65e9cccb1de83a978
Jian-Hua, He <jerry.jianhuahe@gmail.com>
2017-05-07 16:59:06 +0800

    Implement baseConverter.js

--- src/baseConverter.js
+++ src/baseConverter.js
@@ -1,9 +1,23 @@
 import Stack from './Stack';
 
-function baseConverter(decNumber, base) {
-    let remStack = new Stack();
-    let rem;
-    let baseString = '';
+export default function baseConverter(number, base) {
+    let stack = new Stack();
+    let result = '';
     let digits = '0123456789ABCDEF';
-}
 
+    if (base < 1 || base > 16) {
+        throw 'Parameter of base should between 2 and 16';
+    }
+
+    while (number > 0) {
+        let remain = Math.floor(number % base);
+        stack.push(remain);
+        number = Math.floor(number / base);
+    }
+
+    while (!stack.isEmpty()) {
+        result += digits[stack.pop()];
+    }
+
+    return result;
+}
