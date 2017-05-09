edb7106e62fc24b1f41eff386bcd6a5119635dc9
Jian-Hua, He <jerry.jianhuahe@gmail.com>
2017-05-09 22:42:25 +0800

    Add LinkedList js class

---
+++ src/LinkedList.js
@@ -0,0 +1,49 @@
+let length = 0;
+let head = null;
+
+class Node {
+    constructor(element) {
+        this.element = element;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    append(element) {
+
+    }
+
+    insert(position, element) {
+
+    }
+
+    removeAt(position) {
+
+    }
+
+    remove(element) {
+
+    }
+
+    indexOf(element) {
+
+    }
+
+    isEmpty() {
+
+    }
+
+    size() {
+
+    }
+
+    toString() {
+
+    }
+
+    print() {
+        
+    }
+}
+
+export default LinkedList
