4c2eeadd14154fa7568c3dd50cbefd81b6dc3a69
Jian-Hua, He <game09116@gmail.com>
2017-05-13 14:51:00 +0800

    Code refactor

--- src/DoublyLinkedList.js
+++ src/DoublyLinkedList.js
@@ -4,45 +4,46 @@ let _tail = null;
 
 class DoublyLinkedList {
     insert(position, element) {
-        if (position >= 0 && position <= _length) {
-            let node = new Node(element);
-            let current = _head;
-            let previous;
+        let node = new Node(element);
+        let current = _head;
 
-            if (position === 0) {
-                if (!_head) {
-                    _head = node;
-                    _tail = node;
-                } else {
-                    node.next = current;
-                    current.prev = node;
-                    _head = node;
-                }
-            } else if (position === _length) {
-                current = _tail;
-                current.next = node;
-                node.prev = current;
-                _tail = node;
-            } else {
-                let index = 0;
-                while (index < position) {
-                    previous = current;
-                    current = current.next;
-                    index += 1;
-                }
-                node.next = current;
-                previous.next = node;
-
-                current.prev = node;
-                node.prev = previous;
-            }
-
-            _length += 1;
-
-            return true;
-        } else {
+        if (position < 0 && position >= _length) {
             return false;
         }
+
+        if (position === 0) {
+            if (!_head) {
+                _head = node;
+                _tail = node;
+            } else {
+                node.next = current;
+                current.prev = node;
+                _head = node;
+            }
+        } else if (position === _length) {
+            current = _tail;
+            current.next = node;
+            node.prev = current;
+            _tail = node;
+        } else {
+            let index = 0;
+            let previous;
+
+            while (index < position) {
+                previous = current;
+                current = current.next;
+                index += 1;
+            }
+            node.next = current;
+            previous.next = node;
+
+            current.prev = node;
+            node.prev = previous;
+        }
+
+        _length += 1;
+
+        return true;
     }
 
     toString() {
