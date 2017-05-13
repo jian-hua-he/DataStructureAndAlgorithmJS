73bfa4a00f8aa12e239c2a7b37be639cbaae7922
Jian-Hua, He <game09116@gmail.com>
2017-05-13 14:08:08 +0800

    Add DoublyLinkedList

---
+++ src/DoublyLinkedList.js
@@ -0,0 +1,15 @@
+let _length = 0;
+let _head = null;
+let _tail = null;
+
+class DoublyLinkedList {
+
+}
+
+class Node {
+    constructor(element) {
+        this.element = element;
+        this.next = null;
+        this.prev = null;
+    }
+}
