f5878547d01712b015ec500233fdcb092879a548
Jian-Hua, He <game09116@gmail.com>
2017-05-20 16:37:45 +0800

    Create Assembly class

---
+++ src/Assembly.js
@@ -0,0 +1,39 @@
+let _items = {};
+
+class Assembly {
+    add(value) {
+        if ( ! this.has(value)) {
+            _items[value] = value;
+            return true;
+        }
+
+        return false;
+    }
+
+    remove(value) {
+        if ( ! this.has(value)) {
+            delete _items[value];
+            return true;
+        }
+
+        return false;
+    }
+
+    has(value) {
+        return _items.hasOwnProperty(value);
+    }
+
+    clear() {
+        _items = {};
+    }
+
+    size() {
+        return Object.keys(_items).length;
+    }
+
+    values() {
+        return Object.keys(_items);
+    }
+}
+
+export default Assembly
