085ff70e47441c6d038092a0ac3b1e0c0ba51e6a
Jian-Hua, He <game09116@gmail.com>
2017-05-21 15:58:47 +0800

    Use LinkedList to handle conflict keys issue

--- src/HashTable.js
+++ src/HashTable.js
@@ -1,3 +1,5 @@
+import './LinkedList';
+
 function _loseloseHashCode(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i += 1) {
@@ -14,16 +16,73 @@ class HashTable {
 
     put(key, value) {
         let position = _loseloseHashCode(key);
-        this._table[position] = value;
+        if (this._table[position] == undefined) {
+            this._table[position] = new LinkedList();
+        }
+
+        this._table[position].append(new ValuePair(key, value));
     }
 
     remove(key) {
         let position = _loseloseHashCode(key);
-        this._table[position] = undefined;
+        if (this._table[position] !== undefined) {
+            let current = this._table[position].getHead();
+            while (current.next) {
+                if (current.element.key === key) {
+                    this._table[position].remove(current.element);
+                    if (this._table[position].isEmpty()) {
+                        this._table[position] = undefined;
+                    }
+
+                    return true;
+                }
+
+                current = current.next;
+            }
+
+            if (current.element.key === key) {
+                this._table[position].remove(current.element);
+                if (this._table[position].isEmpty()) {
+                    this._table[position] = undefined;
+                }
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
     get(key) {
         let position = _loseloseHashCode(key);
-        return this._table[position];
+        if (this._table[position] == undefined) {
+            return undefined;
+        }
+
+        let current = table[position].getHead();
+        while(current.next) {
+            if (current.element.key === key) {
+                return current.element.value;
+            }
+
+            current = current.next;
+        }
+
+        if (current.element.key === key) {
+            return current.element.value;
+        }
+
+        return undefined;
+    }
+}
+
+class ValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    toString() {
+        return `[${this.key} - ${this.value}]`;
     }
 }
