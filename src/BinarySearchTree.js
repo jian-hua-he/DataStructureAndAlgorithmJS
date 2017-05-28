cabb3b194591fad694cf017da8b69a875ca212bc
Jian-Hua, He <game09116@gmail.com>
2017-05-28 13:49:23 +0800

    Implement insert method

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,10 +1,33 @@
+let _insertNode = (node, newNode) => {
+    if (newNode.key < node.key) {
+        if (node.left === null) {
+            node.left = newNode;
+        } else {
+            _insertNode(node.left, newNode);
+        }
+    } else {
+        if (node.right === null) {
+            node.right = newNode;
+        } else {
+            _insertNode(node.right, newNode);
+        }
+    }
+}
+
 class BinarySearchTree {
     constructor() {
         this._root = null;
     }
 
     insert(key) {
+        let that = this;
+        let newNode = new Node(key);
 
+        if (that._root === null) {
+            that._root = newNode;
+        } else {
+            _insertNode(that._root, newNode);
+        }
     }
 
     search(key) {
@@ -38,9 +61,9 @@ class BinarySearchTree {
 
 class Node {
     constructor(key) {
-        this._key = key;
-        this._left = null;
-        this._right = null;
+        this.key = key;
+        this.left = null;
+        this.right = null;
     }
 }
 
