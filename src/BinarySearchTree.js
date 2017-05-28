c5f492ad99d00da0611f6b4dcdb08584e990ed89
Jian-Hua, He <game09116@gmail.com>
2017-05-28 13:19:53 +0800

    Create BinarySearchTree

---
+++ src/BinarySearchTree.js
@@ -0,0 +1,45 @@
+class BinarySearchTree {
+    constructor() {
+        this._root = null;
+    }
+
+    insert(key) {
+
+    }
+
+    search(key) {
+
+    }
+
+    inOrderTraverse() {
+
+    }
+
+    preOrderTraverse() {
+
+    }
+
+    postOrderTraverse() {
+
+    }
+
+    min() {
+
+    }
+
+    max() {
+
+    }
+
+    remove(key) {
+        
+    }
+}
+
+class Node {
+    constructor(key) {
+        this._key = key;
+        this._left = null;
+        this._right = null;
+    }
+}
