8fec1c4213587b52faf81541032a9bbe0e8436c5
Jian-Hua, He <game09116@gmail.com>
2017-05-29 17:36:16 +0800

    Implement dfs method

--- src/Graph.js
+++ src/Graph.js
@@ -3,11 +3,27 @@ import Queue from './Queue';
 
 let _initializeColor = (vertices) => {
     let color = [];
-    vertices.forEach(function (v) {
+    vertices.forEach((v) => {
         color[v] = 'white';
     });
 
     return color;
+}
+
+let _dfsVisit = (adjList, u, color, callback) => {
+    color[u] = 'grey';
+    if (callback) {
+        callback(u);
+    }
+
+    let neighbors = adjList.get(u);
+    neighbors.forEach((n) => {
+        if (color[n] === 'white') {
+            _dfsVisit(adjList, n, color, callback);
+        }
+    });
+
+    color[u] = 'black';
 }
 
 class Graph {
@@ -37,7 +53,7 @@ class Graph {
             let neighbors = that._adjList.get(u);
             color[u] = 'grey';
 
-            neighbors.forEach(function (n) {
+            neighbors.forEach((n) => {
                 if (color[n] === 'white') {
                     color[n] = 'grey';
                     queue.enqueue(n);
@@ -51,14 +67,25 @@ class Graph {
         }
     }
 
+    dfs(callback) {
+        let that = this;
+        let color = _initializeColor(that._vertices);
+
+        that._vertices.forEach((n) => {
+            if (color[n] === 'white') {
+                _dfsVisit(that._adjList, n, color, callback);
+            }
+        });
+    }
+
     toString() {
         let that = this;
         let result = '';
-        that._vertices.forEach(function (v) {
+        that._vertices.forEach((v) => {
             let neighbors = that._adjList.get(v);
             
             result += v + '->';
-            neighbors.forEach(function (n) {
+            neighbors.forEach((n) => {
                 result += n + ' ';
             });
 
